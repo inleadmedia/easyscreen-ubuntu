779210be452d31019dd655e3965858c07cae2f2c
Martin Cording <mc@inlead.dk>
2020-08-27 09:17:47 +0200

    Depreacte OEM and merge into OOBP

--- README.md
+++ README.md
@@ -1,29 +1,16 @@
 # Setting Up Kiosk
 
-## OEM Installation of the Operating System
+## Installation
 
-Boot from Ubuntu Desktop 18.04.4 LTS as OEM installation on machine and start the installation. Go through the installation process setting the following details.
+Boot from Ubuntu Desktop 18.04.4 LTS on a machine and start the installation. Go through the installation process setting the following details.
 - Keyboard layout: Danish
 - Minimal installation
 - Install third party software: yes
 - Timezone: Copenhagen
 - Erase disk and install Ubuntu
 
-Do the installation in "next-next-next" style until the installation is ready and the system wants to reboot. When the machine computer has rebooted, connect to a WiFi and run the following command:
-```
-wget -qO- https://raw.githubusercontent.com/inleadmedia/easyscreen-ubuntu/new-installation/oem-install.sh | sudo bash
-```
-
-Wait until the terminal ask for the password of the user and input it. The machine will reboot and is ready for installation.
-
-## Installation (OOBP)
-
 Wait for the machine to reboot and proceed with the installation. Go through the installation process setting the following details.
-Note, that this repo is work in process and these details are for the target environment. This repo will be made more modular and portable later.
-- Language: English
-- Install third party software: yes
-- Timezone: Copenhagen
-- Keyboard layout: Danish 
+
 - User information:
   - Name: inlead
   - Computer name: inlead-kiosk-XXX
@@ -31,10 +18,12 @@ Note, that this repo is work in process and these details are for the target env
   - Password: [predefined value]
   - [x] Log in automatically
 
-Do the installation in "next-next-next" style until the installation is ready and the system wants to reboot. When the machine computer has rebooted, run the following command: 
+Do the installation in "next-next-next" style until the installation is ready and the system wants to reboot. When the machine computer has rebooted, connect to a WiFi and run the following command:
 ```
-wget -qO- https://raw.githubusercontent.com/inleadmedia/easyscreen-ubuntu/new-installation/oobp-install.sh | sudo bash
+bash <(wget -O- https://raw.githubusercontent.com/inleadmedia/easyscreen-ubuntu/new-installation/oem-install.sh)
 ```
+
+Wait until the terminal ask for the password of the user and input it.
 
 Wait until the terminal says that it's safe to shutdown. Confirm that you would like to shutdown, and from this point on, the computer is ready for production.
 
--- oem-install.sh
+++ oem-install.sh
@@ -300,11 +300,85 @@ sudo dconf update
 #sudo sh -c "echo \"CLUTTER_PAINT=disable-clipped-redraws:disable-culling\" >> /etc/environment"
 #sudo sh -c "echo \"CLUTTER_VBLANK=True\" >> /etc/environment"
 
+
+### OOBP
+
 echo "$TIMESTAMP # Install and upgrade software"
 sudo apt-get update -qq
 sudo apt-get upgrade -qq
 
-sudo oem-config-prepare --quiet
+echo "$TIMESTAMP # Create user"
+sudo su -c "groupadd kiosk"
+sudo su -c "useradd kiosk -s /bin/bash -d /home/kiosk/ -m -g kiosk"
+
+echo "$TIMESTAMP # Add users to sudo"
+sudo sh -c "echo \"kiosk ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
+sudo sh -c "echo \"inlead ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
+
+echo "$TIMESTAMP # Patch GNNOME shell extension"
+wget -qO- https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/252.diff |  sudo patch /usr/bin/gnome-shell-extension-tool
+
+echo "$TIMESTAMP # Define autologin"
+sudo tee /etc/gdm3/custom.conf >/dev/null <<'EOF'
+# GDM configuration storage
+
+[daemon]
+AutomaticLoginEnable=true
+AutomaticLogin=kiosk
+InitialSetupEnable=false
+
+TimedLoginEnable = true
+TimedLogin = username
+TimedLoginDelay = 10
+
+[security]
+
+[xdmcp]
+
+[chooser]
+
+[debug]
+#Enable=true
+EOF
+
+sudo tee /usr/share/lightdm/lightdm.conf.d/99-kiosk.conf >/dev/null <<'EOF'
+[Seat:*]
+user-session=kiosk
+EOF
+
+sudo -u kiosk tee ~kiosk/.dmrc >/dev/null <<'EOF'
+[Desktop]
+Session=kiosk
+EOF
+
+
+echo "$TIMESTAMP # Network policy"
+sudo tee /etc/polkit-1/localauthority/50-local.d/10-network-manager.pkla >/dev/null <<'EOF'
+[Let user kiosk modify system settings for network]
+Identity=unix-user:kiosk
+Action=org.freedesktop.NetworkManager.settings.modify.system
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+
+echo "$TIMESTAMP # Create autostart"
+sudo -u kiosk mkdir -p /home/kiosk/.config
+sudo -u kiosk mkdir -p /home/kiosk/.config/autostart
+sudo -u kiosk touch /home/kiosk/.config/gnome-initial-setup-done 
+sudo -u kiosk tee /home/kiosk/.config/autostart/conf.desktop >/dev/null <<'EOF'
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Configuration Manager
+Exec=/bin/bash /home/kiosk/conf.sh
+X-GNOME-Autostart-enabled=true
+EOF
+
+sleep 5
+sudo -u kiosk wget -q --output-document=/home/kiosk/conf.sh https://raw.githubusercontent.com/inleadmedia/easyscreen-ubuntu/new-installation/conf.sh
+sudo -u kiosk chmod +x /home/kiosk/conf.sh
+sleep 10
 
 trap : 0
 
@@ -315,5 +389,14 @@ echo >&2 '
 '
 
 sleep 5
+clear
 
-sudo poweroff
+echo "Are you sure you want to poweroff [y/n]"
+while true; do
+	read yn
+    case $yn in
+        [Yy]* ) sudo poweroff;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
