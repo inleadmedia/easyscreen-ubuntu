e62f6952aacaf305e61cbfba1ea0d423d5ee5dda
Martin Cording <mc@inlead.dk>
2020-09-01 13:10:47 +0200

    Move ssh things to conf-hw file

--- conf-hw.sh
+++ conf-hw.sh
@@ -23,6 +23,18 @@ lms_domain="$3";
 branch=$4;
 TMID="$5";
 
+echo "$TIMESTAMP # Fix SSH keys"
+sudo -u kiosk mkdir -p /home/kiosk/.ssh
+sudo -u kiosk wget -q --output-document=/home/kiosk/.ssh/id_rsa.pub http://storage.easyting.dk/ubuntu/sshkey1.txt
+sudo -u kiosk wget -q --output-document=/home/kiosk/.ssh/id_rsa http://storage.easyting.dk/ubuntu/sshkey2.txt
+chmod 0600 /home/kiosk/.ssh/id_rsa
+        
+sudo -u kiosk tee /home/kiosk/.ssh/config >/dev/null <<'EOF'
+Host *
+    UpdateHostKeys yes
+    StrictHostKeyChecking accept-new
+EOF
+
 echo "$TIMESTAMP # Install FF linux app"
 sudo -u kiosk git clone -q -b $branch "git@github.com:inleadmedia/es-linux-apps.git" /home/kiosk/es-linux-apps
 sudo -u kiosk bash /home/kiosk/es-linux-apps/installation/install-nvm.sh
--- kiosk-install.sh
+++ kiosk-install.sh
@@ -27,25 +27,13 @@ sleep 3s
 echo "$TIMESTAMP # Disable Gestures"
 wget -q "https://extensions.gnome.org/extension-data/disable-gestures%40mattbell.com.au.v2.shell-extension.zip"
 sudo -u kiosk gnome-shell-extension-tool -i disable-gestures@mattbell.com.au.v2.shell-extension.zip
-  
+
 echo "$TIMESTAMP # Disable OSK"
 wget -q "https://extensions.gnome.org/extension-data/On_Screen_Keyboard_Button%40bradan.eu.v4.shell-extension.zip"
 sudo -u kiosk gnome-shell-extension-tool -i On_Screen_Keyboard_Button@bradan.eu.v4.shell-extension.zip
-        
+
 wget -q "https://extensions.gnome.org/extension-data/cariboublocker%40git.keringar.xyz.v1.shell-extension.zip"
 sudo -u kiosk gnome-shell-extension-tool -i cariboublocker@git.keringar.xyz.v1.shell-extension.zip
-        
-echo "$TIMESTAMP # Fix SSH keys"
-sudo -u kiosk mkdir -p /home/kiosk/.ssh
-sudo -u kiosk wget -q --output-document=/home/kiosk/.ssh/id_rsa.pub http://storage.easyting.dk/ubuntu/sshkey1.txt
-sudo -u kiosk wget -q --output-document=/home/kiosk/.ssh/id_rsa http://storage.easyting.dk/ubuntu/sshkey2.txt
-chmod 0600 /home/kiosk/.ssh/id_rsa
-        
-sudo -u kiosk tee /home/kiosk/.ssh/config >/dev/null <<'EOF'
-Host *
-    UpdateHostKeys yes
-    StrictHostKeyChecking accept-new
-EOF
 
 echo "$TIMESTAMP # Create autostart"
 sudo -u kiosk tee /home/kiosk/.config/autostart/kiosk.desktop >/dev/null <<'EOF'
